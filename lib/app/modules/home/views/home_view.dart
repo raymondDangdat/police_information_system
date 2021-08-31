3718145a080ce85422a5a089c3a45260f0008bcc
raymondDangdat <raymonddangdat@gmail.com>
2021-08-31 21:15:23 +0100

    Feat: Login screen completed

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:police_info_system/app/controllers/user_controller.dart';
 import 'package:police_info_system/app/widgets/main_drawer.dart';
 
 import '../controllers/home_controller.dart';
 
-class HomeView extends GetView<HomeController> {
+class HomeView extends StatelessWidget {
+  // UserController = Get.put(UserController);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
